ac1152faa658638981a5a5662f465225dec7ca65
Hizi Rabiaa <Hizirabiaa@yahoo.fr>
2023-11-11 23:09:36 +0100

    my commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
+    //my first change
     {
         [Key]
         public int CartItemId { get; set; }
